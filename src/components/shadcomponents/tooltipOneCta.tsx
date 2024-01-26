be52be795a1a30d6e059faea410557ddbf57c01d
2xOu7 <jonathanou1998@gmail.com>
2024-01-26 13:39:19 -0800

    add tooltip examples

--- src/components/shadcomponents/tooltipOneCta.tsx
+++ src/components/shadcomponents/tooltipOneCta.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip'
 import { TooltipArrow } from '@radix-ui/react-tooltip'
+import { Label } from '../ui/label'
+import { Button } from '../ui/button'
 
 export default () => {
   return <TooltipProvider>
     <Tooltip>
       <TooltipTrigger>Hover To Show Example</TooltipTrigger>
-      <TooltipContent>
-        <h3>Add to library</h3>
+      <TooltipContent style={{maxWidth: '200px', overflowWrap: 'break-word'}}>
+        <Label><h3>Tooltip Title</h3></Label>
+        <br/>
+        <Label>Tooltip Description</Label>
         <TooltipArrow />
+        <br/>
+        <br/>
+        <Button style={{float: 'right'}}>CTA</Button>
       </TooltipContent></Tooltip>
   </TooltipProvider>
 }
