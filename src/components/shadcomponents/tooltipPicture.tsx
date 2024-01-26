98ffc77b67038811c6f9d7fcf07876c52ff4b842
2xOu7 <jonathanou1998@gmail.com>
2024-01-26 14:21:46 -0800

    tooltip with picture

--- src/components/shadcomponents/tooltipPicture.tsx
+++ src/components/shadcomponents/tooltipPicture.tsx
@@ -3,45 +3,91 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/
 import { TooltipArrow } from '@radix-ui/react-tooltip'
 import { Label } from '../ui/label'
 import { Button } from '../ui/button'
+import { Compiler, Editor, Error, PropTypes, useView } from 'react-view'
+import presetTypescript from '@babel/preset-typescript'
 
 export default () => {
-  return <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger>
-        Hover To Show Example
-      </TooltipTrigger>
-      <TooltipContent
-        style={{
-          minWidth: "200px",
-          maxWidth: "400px",
-          overflowWrap: "break-word"
-        }}
-      >
-        <Label>
-          <h3>Tooltip Title</h3>
-        </Label>
-        <br />
-        <Label>Tooltip Description</Label>
-        <TooltipArrow />
-        <br />
-        <br />
-        <Button
-          style={{
-            float: "right"
-          }}
-        >
-          CTA 1
-        </Button>
-        <Button
-          style={{
-            float: "right",
-            backgroundColor: "white",
-            color: "black"
-          }}
-        >
-          CTA 2
-        </Button>
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
+  const params = useView({
+    componentName: 'TooltipProvider',
+    props: {
+      children: {
+        value: '<Tooltip>\n' +
+          '    <TooltipTrigger>\n' +
+          '      Hover To Show Example\n' +
+          '    </TooltipTrigger>\n' +
+          '    <TooltipContent\n' +
+          '      style={{\n' +
+          '        minWidth: "200px",\n' +
+          '        maxWidth: "400px",\n' +
+          '        overflowWrap: "break-word"\n' +
+          '      }}\n' +
+          '    >\n' +
+          '      <img\n' +
+          '        style={{\n' +
+          '          marginTop: "1vw"\n' +
+          '        }}\n' +
+          '        src="https://fastly.picsum.photos/id/817/500/300.jpg?hmac=YepWK_ujczi0SlqEvc2ZsSgaDvQrHOvMuSEFXYtOIsY"\n' +
+          '        width={"200"}\n' +
+          '        height={"200"}\n' +
+          '      />\n' +
+          '      <br/>\n' +
+          '      <Label>\n' +
+          '        <h3>Tooltip Title</h3>\n' +
+          '      </Label>\n' +
+          '      <br />\n' +
+          '      <Label>Tooltip Description</Label>\n' +
+          '      <TooltipArrow />\n' +
+          '      <br />\n' +
+          '      <br />\n' +
+          '      <Button\n' +
+          '        style={{\n' +
+          '          float: "right"\n' +
+          '        }}\n' +
+          '      >\n' +
+          '        CTA 1\n' +
+          '      </Button>\n' +
+          '      <Button\n' +
+          '        style={{\n' +
+          '          float: "right",\n' +
+          '          backgroundColor: "white",\n' +
+          '          color: "black"\n' +
+          '        }}\n' +
+          '      >\n' +
+          '        CTA 2\n' +
+          '      </Button>\n' +
+          '    </TooltipContent>\n' +
+          '  </Tooltip>',
+        type: PropTypes.ReactNode,
+        description: 'Implementation of Tooltip',
+        imports: {
+          '@/components/ui/tooltip': {
+            named: ['Tooltip',
+              'TooltipContent',
+              'TooltipProvider',
+              'TooltipTrigger'],
+          },
+          '@radix-ui/react-tooltip': {
+            named: ['TooltipArrow'],
+          },
+          '@/components/ui/label': {
+            named: ['Label'],
+          },
+          '@/components/ui/button': {
+            named: ['Button'],
+          },
+        },
+      },
+    },
+    scope: {
+      Tooltip, TooltipContent, TooltipProvider, TooltipTrigger, TooltipArrow, Label, Button,
+    },
+  })
+
+  return (
+    <React.Fragment>
+      <Compiler {...params.compilerProps} presets={[presetTypescript]} />
+      <Editor {...params.editorProps} language={'tsx'} />
+      <Error {...params.errorProps} />
+    </React.Fragment>
+  )
 }
