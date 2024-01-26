aeae23e8cfd958300202c8348c94123e85d1c8fc
2xOu7 <jonathanou1998@gmail.com>
2024-01-26 15:25:35 -0800

    clean up code

--- src/components/shadcomponents/banner.tsx
+++ src/components/shadcomponents/banner.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import { Button } from '../ui/button'
 
 export default () => {
   return <Card>
-    <CardHeader>
-      <CardTitle>Card Title</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <p>Card Content</p>
-    </CardContent>
-    <CardFooter className={'flex justify-end'}>
-      <Button style={{ backgroundColor: 'white', color: 'black' }}>CTA 1</Button>
-      <Button>CTA 2</Button>
-    </CardFooter>
+    <div style={{ marginTop: '1vh' }} className={'grid grid-cols-2'}>
+      <div>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </div>
+      <div>
+        <div className={'flex justify-end items-center'} style={{marginTop: '5vh', marginRight: '1vw'}}>
+          <Button style={{ backgroundColor: 'white', color: 'black' }}>CTA 1</Button>
+          <Button>CTA 2</Button>
+        </div>
+      </div>
+    </div>
   </Card>
+
 }
