7ddca5ad64a627f1394d70dda5c81e8966d382f2
2xOu7 <jonathanou1998@gmail.com>
2024-01-26 21:46:30 -0800

    add navigation menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { Menu } from 'semantic-ui-react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <div>
+    <Menu size={'small'} secondary icon={'labeled'}>
+      <Menu.Item
+        name={'campaigns'}
+        active={false}
+      >
+        shadcn
+      </Menu.Item>
+    </Menu>
+    <Component {...pageProps} />
+  </div>
 }
