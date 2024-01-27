ed507984ad84c2b357b4e9ecebcac372e7c8036d
2xOu7 <jonathanou1998@gmail.com>
2024-01-26 22:01:14 -0800

    finish adding menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,34 @@
 import '@/styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import type { AppProps } from 'next/app'
-import { Menu } from 'semantic-ui-react'
+import { Icon, Menu, Segment } from 'semantic-ui-react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
-    <Menu size={'small'} secondary icon={'labeled'}>
-      <Menu.Item
-        name={'campaigns'}
-        active={false}
-      >
-        shadcn
-      </Menu.Item>
-    </Menu>
+    <Segment>
+      <Menu size={'small'} secondary icon={'labeled'}>
+        <Menu.Item
+          name={'shadcn'}
+          active={false}
+          onClick={() => {
+            window.location.href = '/'
+          }}
+        >
+          <Icon name={'file'} />
+          README
+        </Menu.Item>
+        <Menu.Item
+          name={'shadcn'}
+          active={false}
+          onClick={() => {
+            window.location.href = '/shadcn'
+          }}
+        >
+          <Icon name={'code'} />
+          ShadCN Examples
+        </Menu.Item>
+      </Menu>
+    </Segment>
     <Component {...pageProps} />
   </div>
 }
